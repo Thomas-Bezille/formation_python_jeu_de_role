d42fe489cab0003084149d9d2bdcfa5448fb8fc1
thomas <bezille.thomasb@gmail.com>
2024-10-30 16:34:31 +0100

    📦 NEW: Managing the attack action

--- game.py
+++ game.py
@@ -1,12 +1,23 @@
 import random
 import sys
 
+ATTACK = "1"
+POTION = "2"
+
 life_player = 50
 life_enemy = 50
 
 potion_player = 3
 
-print("//////////----- JEU DE ROLE -----//////////")
+print("\n//////////----- JEU DE ROLE -----//////////")
 
 while life_player > 0 or life_enemy > 0:
-    action_player = input("\nSouhaitez-vous attaquer ⚔️ (1) ou utiliser une potion 🧪 (2) ? >>> ")
+    action_player = input("\nSouhaitez-vous attaquer ⚔️  (1) ou utiliser une potion 🧪 (2) ? >>> ")
+    if action_player != ATTACK and action_player != POTION:
+        print("Sasissez (1) pour attaquer ⚔️  ou (2) pour utiliser une potion 🧪")
+    
+    if action_player == ATTACK:
+        power = random.randint(5, 10)
+        life_enemy -= power
+        print(f"Vous avez infligé {power} points de dégats à l'ennemi ⚔️")
+        print(life_enemy)
