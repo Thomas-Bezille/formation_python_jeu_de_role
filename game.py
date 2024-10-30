c3a1a3990b343bb468f69e3714506e070cb0c339
thomas <bezille.thomasb@gmail.com>
2024-10-30 17:16:44 +0100

    📦 NEW: Skip round management if the player use a potion

--- game.py
+++ game.py
@@ -11,30 +11,40 @@ potion_player = 3
 
 skip_next_round = False
 
-print("\n//////////----- JEU DE ROLE -----//////////")
+print(f"\n{"/" * 20}{"-" * 5} JEU DE ROLE {"-" * 5}{"/" * 20}")
 
-while life_player > 0 or life_enemy > 0:
-    action_player = input("\nSouhaitez-vous attaquer ⚔️  (1) ou utiliser une potion 🧪 (2) ? >>> ")
-    if action_player != ATTACK and action_player != POTION:
-        print("Sasissez (1) pour attaquer ⚔️  ou (2) pour utiliser une potion 🧪")
-    
-    if action_player == ATTACK:
-        power = random.randint(5, 10)
-        life_enemy -= power
-        print(f"Vous avez infligé {power} points de dégats à l'ennemi ⚔️")
-        print(life_enemy)
-    
-    if action_player == POTION and potion_player > 0:
-        heal = random.randint(15, 20)
-        life_player += heal
-        potion_player -= 1
-        print(f"Vous avez récupérer {heal} points de vie ❤️ ({potion_player} restante(s))")
-        skip_next_round = True
-        print(life_player)
-    elif potion_player <= 0:
-        continue
+while True:
+    if skip_next_round == False:
+        action_player = input("\nSouhaitez-vous attaquer ⚔️  (1) ou utiliser une potion 🧪 (2) ? >>> ")
+        
+        if action_player != ATTACK and action_player != POTION:
+                print("Sasissez (1) pour attaquer ⚔️  ou (2) pour utiliser une potion 🧪")
+                continue
+        
+        if action_player == ATTACK:
+            power = random.randint(5, 10)
+            life_enemy -= power
+            print(f"Vous avez infligé {power} points de dégats à l'ennemi ⚔️")
+        
+        if action_player == POTION and potion_player > 0:
+            heal = random.randint(15, 20)
+            life_player += heal
+            potion_player -= 1
+            print(f"Vous avez récupérer {heal} points de vie ❤️ ({potion_player} restante(s))")
+            skip_next_round = True
+        elif action_player == POTION and potion_player <= 0:
+            print("Vous n'avez plus de potion...")
+            continue
+    else:
+        print("Vous passez votre tour... 💤")
+        skip_next_round = False
     
     # Enemy Attack (He can only attack, he has no potion)
     power = random.randint(5, 15)
     life_player -= power
     print(f"L'ennemi vous a infligé {power} points de dégats ⚔️")
+    
+    # Resume
+    print(f"Il vous reste {life_player} points de vie.")
+    print(f"Il reste {life_enemy} points de vie à l'ennemi.")
+    print("-" * 50)
